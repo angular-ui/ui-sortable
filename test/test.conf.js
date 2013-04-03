27d85e21cb96f49ed53cb48ed58841804e3fb4f9
Dean Sofer <dsofer@paxata.com>
2013-04-02 23:03:05 -0700

    Initial structure of ui-sortable

---
+++ test/test.conf.js
@@ -0,0 +1,13 @@
+basePath = '..';
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'components/jquery/jquery.js',
+  'components/jquery-ui/ui/jquery-ui.custom.js',
+  'components/angular/angular.js',
+  'components/angular-mocks/angular-mocks.js',
+  'src/sortable.js',
+  'test/*.spec.js'
+];
+singleRun = true;
+browsers = [ 'Chrome' ];
