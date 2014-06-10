191f70a43b4a91f02d7f8ccb0f6fb8c3469a0935
Thodoris Greasidis <thgreasi@gmail.com>
2014-06-11 01:05:06 +0300

    test(sortable): add test for dragging items from parent list into nested list

--- test/sortable.e2e.multi.spec.js
+++ test/sortable.e2e.multi.spec.js
@@ -353,11 +353,11 @@ describe('uiSortable', function() {
 
         elementTree = $compile(''.concat(
           '<ul ui-sortable="sortableOptions" ng-model="items" class="apps-container outterList" style="float: left;margin-left: 10px;padding-bottom: 10px;">',
-            '<li ng-repeat="item in items track by $index">',
+            '<li ng-repeat="item in items">',
               '<div>',
                 '<span class="itemContent lvl1ItemContent">{{item.text}}</span>',
                 '<ul ui-sortable="sortableOptions" ng-model="item.items" class="apps-container innerList" style="margin-left: 10px;padding-bottom: 10px;">',
-                  '<li ng-repeat="i in item.items track by $index">',
+                  '<li ng-repeat="i in item.items">',
                     '<span class="itemContent lvl2ItemContent">{{i.text}}</span>',
                   '</li>',
                 '</ul>',
@@ -388,9 +388,9 @@ describe('uiSortable', function() {
 
         host.append(elementTree);
 
-        li1 = elementTree.find('.innerList:last').find(':last');
         // this should drag the item out of the list and
         // the item should return back to its original position
+        li1 = elementTree.find('.innerList:last').find(':last');
         li1.simulate('drag', { dx: -200, moves: 30 });
         expect($rootScope.items.map(function(x){ return x.text; }))
           .toEqual(['Item 1', 'Item 2']);
@@ -405,6 +405,8 @@ describe('uiSortable', function() {
         expect($rootScope.items[1].items.map(function(x){ return x.text; }))
           .toEqual(listInnerContent(elementTree.find('.innerList:eq(1)'), '.lvl2ItemContent'));
 
+        // this should drag the item from the inner list and
+        // drop it to the outter list
         li1 = elementTree.find('.innerList:last').find(':last');
         li2 = elementTree.find('> li:last');
         dy = EXTRA_DY_PERCENTAGE * li1.outerHeight() + (li2.position().top - li1.position().top);
@@ -425,6 +427,25 @@ describe('uiSortable', function() {
           .toEqual(['Item 2.1']);
         expect($rootScope.items[2].items.map(function(x){ return x.text; }))
           .toEqual(listInnerContent(elementTree.find('.innerList:eq(2)'), '.lvl2ItemContent'));
+
+        // this should drag the item from the outter list and
+        // drop it to the inner list
+        li1 = elementTree.find('> li:first');
+        li2 = elementTree.find('.innerList:last').find(':last');
+        dy = -EXTRA_DY_PERCENTAGE * li1.outerHeight() + (li2.position().top - li1.position().top);
+        li1.simulate('drag', { dy: dy });
+        expect($rootScope.items.map(function(x){ return x.text; }))
+          .toEqual(['Item 2.2', 'Item 2']);
+        expect($rootScope.items.map(function(x){ return x.text; }))
+          .toEqual(listInnerContent(elementTree, '.lvl1ItemContent'));
+        expect($rootScope.items[0].items.map(function(x){ return x.text; }))
+          .toEqual([]);
+        expect($rootScope.items[0].items.map(function(x){ return x.text; }))
+          .toEqual(listInnerContent(elementTree.find('.innerList:eq(0)'), '.lvl2ItemContent'));
+        expect($rootScope.items[1].items.map(function(x){ return x.text; }))
+          .toEqual(['Item 1', 'Item 2.1']);
+        expect($rootScope.items[1].items.map(function(x){ return x.text; }))
+          .toEqual(listInnerContent(elementTree.find('.innerList:eq(1)'), '.lvl2ItemContent'));
 
         $(elementTree).remove();
       });
