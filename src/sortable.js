2fb4240d151b8d9c02cb4ae046b70431a85c7559
Thodoris Greasidis <thgreasi@gmail.com>
2014-07-09 00:51:06 +0300

    feat(sortable): update local variable with active options

--- src/sortable.js
+++ src/sortable.js
@@ -241,6 +241,7 @@ angular.module('ui.sortable', [])
                     value = wrappers[key](value);
                   }
                   
+                  opts[key] = value;
                   element.sortable('option', key, value);
                 });
               }
