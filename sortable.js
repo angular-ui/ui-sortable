5d4ad72e13c9f68c21b6bef3ccbb4562522036c5
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-10-14 19:52:11 +0000

    Travis commit : build 322

--- sortable.js
+++ sortable.js
@@ -30,7 +30,21 @@ angular.module('ui.sortable', [])
             return helperOption === 'clone' || (typeof helperOption === 'function' && ui.item.sortable.isCustomHelperUsed());
           }
 
+          // thanks jquery-ui
+          function isFloating (item) {
+            return (/left|right/).test(item.css('float')) || (/inline|table-cell/).test(item.css('display'));
+          }
+
+          function afterStop(e, ui) {
+            ui.item.sortable._destroy();
+          }
+
           var opts = {};
+
+          // directive specific options
+          var directiveOpts = {
+            'ui-floating': undefined
+          };
 
           var callbacks = {
             receive: null,
@@ -44,7 +58,7 @@ angular.module('ui.sortable', [])
             helper: null
           };
 
-          angular.extend(opts, uiSortableConfig, scope.$eval(attrs.uiSortable));
+          angular.extend(opts, directiveOpts, uiSortableConfig, scope.$eval(attrs.uiSortable));
 
           if (!angular.element.fn || !angular.element.fn.jquery) {
             $log.error('ui.sortable: jQuery should be included before AngularJS!');
@@ -67,9 +81,19 @@ angular.module('ui.sortable', [])
             });
 
             callbacks.start = function(e, ui) {
+              if (opts['ui-floating'] === 'auto') {
+                // since the drag has started, the element will be
+                // absolutely positioned, so we check its siblings
+                var siblings = ui.item.siblings();
+                angular.element(e.target).data('ui-sortable').floating = isFloating(siblings);
+              }
+
               // Save the starting position of dragged item
               ui.item.sortable = {
+                model: ngModel.$modelValue[ui.item.index()],
                 index: ui.item.index(),
+                source: ui.item.parent(),
+                sourceModel: ngModel.$modelValue,
                 cancel: function () {
                   ui.item.sortable._isCanceled = true;
                 },
@@ -80,7 +104,12 @@ angular.module('ui.sortable', [])
                   return !!ui.item.sortable._isCustomHelperUsed;
                 },
                 _isCanceled: false,
-                _isCustomHelperUsed: ui.item.sortable._isCustomHelperUsed
+                _isCustomHelperUsed: ui.item.sortable._isCustomHelperUsed,
+                _destroy: function () {
+                  angular.forEach(ui.item.sortable, function(value, key) {
+                    ui.item.sortable[key] = undefined;
+                  });
+                }
               };
             };
 
@@ -121,7 +150,9 @@ angular.module('ui.sortable', [])
               // the value will be overwritten with the old value
               if(!ui.item.sortable.received) {
                 ui.item.sortable.dropindex = ui.item.index();
-                ui.item.sortable.droptarget = ui.item.parent();
+                var droptarget = ui.item.parent();
+                ui.item.sortable.droptarget = droptarget;
+                ui.item.sortable.droptargetModel = droptarget.scope().$eval(droptarget.attr('ng-model'));
 
                 // Cancel the sort (let ng-repeat do the sort for us)
                 // Don't cancel if this is the received list because it has
@@ -231,11 +262,24 @@ angular.module('ui.sortable', [])
               // is still bound to the directive's element
               if (!!element.data('ui-sortable')) {
                 angular.forEach(newVal, function(value, key) {
-                  if(callbacks[key]) {
+                  // if it's a custom option of the directive,
+                  // handle it approprietly
+                  if (key in directiveOpts) {
+                    if (key === 'ui-floating' && (value === false || value === true)) {
+                      element.data('ui-sortable').floating = value;
+                    }
+
+                    opts[key] = value;
+                    return;
+                  }
+
+                  if (callbacks[key]) {
                     if( key === 'stop' ){
                       // call apply after stop
                       value = combineCallbacks(
                         value, function() { scope.$apply(); });
+
+                      value = combineCallbacks(value, afterStop);
                     }
                     // wrap the callback
                     value = combineCallbacks(callbacks[key], value);
@@ -243,6 +287,7 @@ angular.module('ui.sortable', [])
                     value = wrappers[key](value);
                   }
                   
+                  opts[key] = value;
                   element.sortable('option', key, value);
                 });
               }
@@ -250,6 +295,9 @@ angular.module('ui.sortable', [])
 
             angular.forEach(callbacks, function(value, key) {
               opts[key] = combineCallbacks(value, opts[key]);
+              if( key === 'stop' ){
+                opts[key] = combineCallbacks(opts[key], afterStop);
+              }
             });
 
           } else {
