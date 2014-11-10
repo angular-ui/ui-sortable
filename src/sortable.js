bef515e8f8decdf999d67e923d5745e4ceba266c
Brian Graham <bgrambo@gmail.com>
2014-11-10 18:46:13 +0000

    chore(sortable): clean up whitespace

--- src/sortable.js
+++ src/sortable.js
@@ -285,7 +285,7 @@ angular.module('ui.sortable', [])
                   } else if (wrappers[key]) {
                     value = wrappers[key](value);
                   }
-                  
+
                   opts[key] = value;
                   element.sortable('option', key, value);
                 });
