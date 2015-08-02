d0a71408bf4c891bf71edd6dfdc917fa70619474
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-08-02 18:07:49 +0000

    Travis commit : build 420

--- dist/sortable.js
+++ dist/sortable.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-sortable - This directive allows you to jQueryUI Sortable.
- * @version v0.13.4 - 2015-07-28
+ * @version v0.13.4 - 2015-08-02
  * @link http://angular-ui.github.com
  * @license MIT
  */
